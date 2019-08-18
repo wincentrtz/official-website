a02e870642c9141414690bd65fb93f776086c811
wincentrtz <wincentang3636@gmail.com>
2019-08-18 21:10:35 +0700

    construct home page layout

---
+++ src/pages/home/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomePageContainer = styled.div`
+  display: flex;
+`;
+export const ImageSection = styled.div`
+  flex-grow: 1;
+`;
+export const ContentSection = styled.div`
+  flex-grow: 1;
+`;
